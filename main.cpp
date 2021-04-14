dd0f85f5db515dcac2d8127061592a69f6108c4b
lyxell <alyxell@kth.se>
2021-04-14 04:41:25 +0200

    Add multiline editor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include "imgui-boilerplate/window.hpp"
 #include "imgui.h"
 
+void render_editor() {
+
+    ImGui::Begin("Editor");
+    // Note: we are using a fixed-sized buffer for simplicity here. See ImGuiInputTextFlags_CallbackResize
+    // and the code in misc/cpp/imgui_stdlib.h for how to setup InputText() for dynamically resizing strings.
+    static char text[1024 * 16] =
+        "/*\n"
+        " The Pentium F00F bug, shorthand for F0 0F C7 C8,\n"
+        " the hexadecimal encoding of one offending instruction,\n"
+        " more formally, the invalid operand with locked CMPXCHG8B\n"
+        " instruction bug, is a design flaw in the majority of\n"
+        " Intel Pentium, Pentium MMX, and Pentium OverDrive\n"
+        " processors (all in the P5 microarchitecture).\n"
+        "*/\n\n"
+        "label:\n"
+        "\tlock cmpxchg8b eax\n";
+
+    static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
+    ImGui::CheckboxFlags("ImGuiInputTextFlags_ReadOnly", &flags, ImGuiInputTextFlags_ReadOnly);
+    ImGui::CheckboxFlags("ImGuiInputTextFlags_AllowTabInput", &flags, ImGuiInputTextFlags_AllowTabInput);
+    ImGui::CheckboxFlags("ImGuiInputTextFlags_CtrlEnterForNewLine", &flags, ImGuiInputTextFlags_CtrlEnterForNewLine);
+    ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16), flags);
+    ImGui::End();
+}
+
 void render_dockspace() {
 
     bool p_open = true;
@@ -88,6 +113,7 @@ int main() {
         window::start_frame();
 
         render_dockspace();
+        render_editor();
 
         ImGui::ShowDemoWindow(&show_demo_window);
         {
