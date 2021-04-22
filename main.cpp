0752ae9bbe5556292c060aae93ee3ed398650a35
lyxell <alyxell@kth.se>
2021-04-22 20:26:06 +0200

    Render repairable nodes

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 #include "datalog-repair/program.hpp"
 #include <memory>
 
+std::vector<std::tuple<std::string,int,int>> repairable_nodes;
 std::vector<std::pair<int,int>> repairs;
 
 void render_ast(std::shared_ptr<sjp::tree_node> t, size_t pos) {
     if (t) {
         ImGui::Indent();
-        if (t->get_name() == "equals_expression") {
-            repairs.emplace_back(
-                    t->get_start_token(),
-                    t->get_end_token());
+        for (auto& [a,b,c] : repairable_nodes) {
+            if (a == t->get_name() && b == t->get_start_token() && c == t->get_end_token()) {
+                repairs.emplace_back(
+                        t->get_start_token(),
+                        t->get_end_token());
+            }
         }
         if (t->get_start_token() <= pos && pos <= t->get_end_token()) {
             ImGui::Text("%s", t->get_name().c_str());
@@ -64,6 +67,7 @@ int main() {
             program.add_string("Example.java", output.c_str());
             program.run();
             ast = program.get_ast("Example.java");
+            repairable_nodes = program.get_repairable_nodes("Example.java");
             prev_lines = ed.lines;
         }
     }
