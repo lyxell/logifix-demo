8bee4bac6955d01e9d8a6e74d4d0ac8bf6a3a760
lyxell <alyxell@kth.se>
2021-04-28 03:33:31 +0200

    Clean up

--- main.cpp
+++ main.cpp
@@ -7,32 +7,6 @@
 #include <memory>
 #include <map>
 
-/*
-std::map<std::tuple<std::string,int,int>,std::string> repairable_nodes;
-std::map<std::tuple<std::string,int,int>,std::string> to_string;
-std::map<std::tuple<std::string,int,int>,std::vector<std::string>> declared_vars;
-std::vector<std::tuple<int,int,std::string>> rewrites;
-
-void find_rewrites(std::shared_ptr<sjp::tree_node> node) {
-    if (!node) return;
-    auto it = repairable_nodes.find(std::tuple(node->get_name(),
-                                         node->get_start_token(),
-                                         node->get_end_token()));
-    if (it != repairable_nodes.end()) {
-        auto [k, v] = *it;
-        rewrites.emplace_back(node->get_start_token(),
-                              node->get_end_token(), v);
-    }
-    for (auto [symbol, child] : node->get_parent_of()) {
-        find_rewrites(child);
-    }
-    for (auto [symbol, children] : node->get_parent_of_list()) {
-        for (auto child : children) {
-            find_rewrites(child);
-        }
-    }
-}*/
-
 int main() {
     std::string filename = "Example.java";
     repair repair_program;
