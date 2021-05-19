ea2af93e471dea818eb0d4828d0a0164af5f23d3
lyxell <alyxell@kth.se>
2021-05-19 10:13:20 +0200

    Update dependencies, more performance logging

--- program.cpp
+++ program.cpp
@@ -21,29 +21,33 @@ void run(state* s) {
     //rep.run();
     program->add_string(filename, data.c_str());
     auto t2 = hclock::now();
-    std::cout << "Time difference:  "
+    std::cout << "Parsing took:  "
               << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << " milliseconds" << std::endl;
     program->run();
-
+    auto t3 = hclock::now();
+    std::cout << "Analysis took:  "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2)
+                     .count()
+              << " milliseconds" << std::endl;
 
     auto repairs = program->get_possible_rewrites(filename);
     // temporary way to not include overlapping repairs
     decltype(repairs) filtered_repairs;
     for (size_t i = 0; i < repairs.size(); i++) {
-        auto [ai, bi, ci, di] = repairs[i];
+        auto [ri, ai, bi, ci, di] = repairs[i];
         bool contained = false;
         for (size_t j = 0; j < repairs.size(); j++) {
             if (i == j)
                 continue;
-            auto [aj, bj, cj, dj] = repairs[j];
+            auto [rj, aj, bj, cj, dj] = repairs[j];
             if ((aj < ai && bj >= bi) || (aj <= ai && bj > bi)) {
                 contained = true;
             }
         }
         if (!contained) {
-            filtered_repairs.emplace_back(ai, bi, ci, di);
+            filtered_repairs.emplace_back(ri, ai, bi, ci, di);
         }
     }
 
@@ -51,7 +55,7 @@ void run(state* s) {
         const std::lock_guard<std::mutex> lock(s->mutex);
         if (filtered_repairs.size() == s->repairs.size()) {
             for (size_t i = 0; i < filtered_repairs.size(); i++) {
-                auto& [start, end, replacement, message] = filtered_repairs[i];
+                auto& [rn, start, end, replacement, message] = filtered_repairs[i];
                 s->repairs[i].start = start;
                 s->repairs[i].end = end;
                 s->repairs[i].message = message;
@@ -59,7 +63,7 @@ void run(state* s) {
             }
         } else {
             s->repairs = {};
-            for (auto& [start, end, replacement, message] : filtered_repairs) {
+            for (auto& [rn, start, end, replacement, message] : filtered_repairs) {
                 repair r;
                 r.start = start;
                 r.end = end;
